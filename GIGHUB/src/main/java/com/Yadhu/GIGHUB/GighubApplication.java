ec11ed817ce28ea173dda31b7f3154ea5d1b82db
yadhu <yadhu.raintech@mail.com>
2025-04-08 10:30:25 +0530

    First  commit for Gighub

---
+++ GIGHUB/src/main/java/com/Yadhu/GIGHUB/GighubApplication.java
@@ -0,0 +1,13 @@
+package com.Yadhu.GIGHUB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GighubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GighubApplication.class, args);
+	}
+
+}
---
+++ GIGHUB/src/test/java/com/Yadhu/GIGHUB/GighubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yadhu.GIGHUB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GighubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
